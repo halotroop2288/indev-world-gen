7096b678e3c21a6d049eb81707322f33e4321e74
Amb0s <ambos_ambos@yahoo.com>
2019-11-22 01:40:13 +0100

    Initial commit

---
+++ src/main/java/ambos/indevworldgen/IndevWorldGen.java
@@ -0,0 +1,10 @@
+package ambos.indevworldgen;
+
+import net.fabricmc.api.ModInitializer;
+
+public class IndevWorldGen implements ModInitializer {
+	@Override
+	public void onInitialize() {
+		System.out.println("Hello Fabric world!");
+	}
+}
