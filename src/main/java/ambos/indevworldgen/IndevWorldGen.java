6dd66ba42cb15e15cb96551e2f09ca23f9f88901
Amb0s <ambos_ambos@yahoo.com>
2019-11-24 01:23:13 +0100

    Created structure
    - Added NBT file
    - Registered structure

--- src/main/java/ambos/indevworldgen/IndevWorldGen.java
+++ src/main/java/ambos/indevworldgen/IndevWorldGen.java
@@ -2,10 +2,20 @@ package ambos.indevworldgen;
 
 import ambos.indevworldgen.gen.IndevChunkGeneratorConfig;
 import ambos.indevworldgen.gen.IndevChunkGeneratorType;
+import ambos.indevworldgen.gen.feature.IndevHouseFeature;
+import ambos.indevworldgen.structure.IndevHouseGenerator;
 import ambos.indevworldgen.worldtype.OldWorldType;
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.structure.StructurePieceType;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.decorator.ChanceDecoratorConfig;
+import net.minecraft.world.gen.decorator.Decorator;
+import net.minecraft.world.gen.feature.DefaultFeatureConfig;
+import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.StructureFeature;
 
 public class IndevWorldGen implements ModInitializer {
 	private static final String MOD_ID = "indevworldgen";
@@ -13,12 +23,27 @@ public class IndevWorldGen implements ModInitializer {
 	public static IndevChunkGeneratorType INSTANCE;
 	
 	static OldWorldType<?> loadMeOnClientPls; // make sure world types are loaded on client by referencing a field in onInitialize()
+
+	public static final StructurePieceType indevHouseStructurePieceType = Registry.register(Registry.STRUCTURE_PIECE, "indev_house_piece", IndevHouseGenerator.Piece::new);
+	public static final StructureFeature<DefaultFeatureConfig> indevHouseFeature = Registry.register(Registry.FEATURE, "indev_house_feature", new IndevHouseFeature());
+	public static final StructureFeature<?> indevHouseStructure = Registry.register(Registry.STRUCTURE_FEATURE, "indev_house_structure", indevHouseFeature);
 	
 	@Override
 	public void onInitialize() {
 		loadMeOnClientPls = OldWorldType.INDEV;
 		
 		INSTANCE = Registry.register(Registry.CHUNK_GENERATOR_TYPE, IndevWorldGen.id("old_indev"), new IndevChunkGeneratorType(false, () -> new IndevChunkGeneratorConfig()));
+
+		Feature.STRUCTURES.put("My Awesome Feature", indevHouseFeature);
+
+		for(Biome biome : Registry.BIOME)
+		{
+			if(biome.getCategory() != Biome.Category.OCEAN && biome.getCategory() != Biome.Category.RIVER)
+			{
+				biome.addStructureFeature(indevHouseFeature, new DefaultFeatureConfig());
+				biome.addFeature(GenerationStep.Feature.SURFACE_STRUCTURES, Biome.configureFeature(indevHouseFeature, new DefaultFeatureConfig(), Decorator.CHANCE_PASSTHROUGH, new ChanceDecoratorConfig(0)));
+			}
+		}
 	}
 
 	public static Identifier id(String id) {
---
+++ src/main/java/ambos/indevworldgen/gen/feature/IndevHouseFeature.java
@@ -0,0 +1,76 @@
+package ambos.indevworldgen.gen.feature;
+
+import ambos.indevworldgen.IndevWorldGen;
+import ambos.indevworldgen.structure.IndevHouseGenerator;
+import net.minecraft.structure.StructureManager;
+import net.minecraft.structure.StructureStart;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MutableIntBoundingBox;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.minecraft.world.gen.feature.AbstractTempleFeature;
+import net.minecraft.world.gen.feature.DefaultFeatureConfig;
+import net.minecraft.world.gen.feature.StructureFeature;
+
+import java.util.Random;
+
+public class IndevHouseFeature extends AbstractTempleFeature<DefaultFeatureConfig> {
+    public IndevHouseFeature()
+    {
+        super(DefaultFeatureConfig::deserialize);
+    }
+
+    @Override
+    public int getRadius()
+    {
+        return 0;
+    }
+
+    @Override
+    public StructureStartFactory getStructureStartFactory()
+    {
+        return IndevHouseStart::new;
+    }
+
+    @Override
+    public String getName()
+    {
+        return "indev_house";
+    }
+
+    @Override
+    public int getSeedModifier()
+    {
+        return 3;
+    }
+
+    @Override
+    public boolean shouldStartAt(ChunkGenerator<?> chunkGenerator_1, Random random_1, int int_1, int int_2)
+    {
+        return true;
+    }
+
+    public static class IndevHouseStart extends StructureStart
+    {
+
+        public IndevHouseStart(StructureFeature<?> structureFeature_1, int int_1, int int_2, Biome biome_1, MutableIntBoundingBox mutableIntBoundingBox_1, int int_3, long long_1)
+        {
+            super(structureFeature_1, int_1, int_2, biome_1, mutableIntBoundingBox_1, int_3, long_1);
+        }
+
+        @Override
+        public void initialize(ChunkGenerator<?> chunkGenerator, StructureManager structureManager, int chunkX, int chunkZ, Biome biome)
+        {
+            DefaultFeatureConfig defaultFeatureConfig = chunkGenerator.getStructureConfig(biome, IndevWorldGen.indevHouseFeature);
+
+            int x = chunkX * 16;
+            int z = chunkZ * 16;
+
+            BlockPos startingPos = new BlockPos(x, 0, z);
+            BlockRotation blockRotation = BlockRotation.values()[this.random.nextInt(BlockRotation.values().length)];
+            IndevHouseGenerator.addParts(structureManager, startingPos, blockRotation, this.children, this.random, defaultFeatureConfig);
+            this.setBoundingBoxFromChildren();
+        }
+    }
+}
